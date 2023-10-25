c3d9073a844813fe946599ad9037af51b8f1796d
aviral <avi14320@gmail.com>
2023-10-26 02:08:49 +0530

    file updated

--- server/index.js
+++ server/index.js
@@ -10,6 +10,16 @@ import dalleRoutes from "./routes/dalleRoutes.js";
 dotenv.config();
 
 const app = express();
+
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
+
 app.use(cors());
 // app.use(
 //   cors({
